207a1c5e32630f501d08229ca90cf307cb2439ab
Mohamed Mazy <mazymohamed06@gmail.com>
2020-12-30 22:26:10 +0000

    check errors signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
+const {signUpErrors} = require("../utils/errors.utils");
 
 const maxAge = 3 * 24 * 60 * 1000
 const createToken = (id) => {
@@ -14,7 +15,8 @@ module.exports.signUp = async (req, res) => {
         const user = await UserModel.create({pseudo, email, password})
         res.status(201).json({user: user._id})
     } catch (err) {
-        res.status(200).send({err})
+        const errors = signUpErrors(err)
+        res.status(200).send({errors})
     }
 }
 
--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
             validate: [isEmail],
+            unique: true,
             lowercase: true,
             trim: true
         },
