0fd499c162d43cb1bc1b3f32471861b94330ff2a
Mohamed Mazy <mazymohamed06@gmail.com>
2021-01-10 18:47:04 +0000

    add popup following

--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -3,12 +3,18 @@ import LeftNav from "../LeftNav";
 import {useDispatch, useSelector} from "react-redux";
 import UploadImg from "./UploadImg";
 import {updateBio} from "../../actions/user.actions";
+import {dateParser} from "../Utils";
 
 const UpdateProfil = () => {
     const [bio, setBio] = useState('')
     const [updateForm, setUpdateForm] = useState(false)
     const userData = useSelector((state) => state.userReducer)
+    const usersData = useSelector((state) => state.usersReducer)
+
     const dispatch = useDispatch()
+    const [followingPopup, setFollowingPopup] = useState(false)
+    const [followersPopup, setFollowersPopup] = useState(false)
+
 
     const handleUpdate = () => {
         dispatch(updateBio(userData._id, bio))
@@ -37,13 +43,41 @@ const UpdateProfil = () => {
                         {updateForm && (
                             <>
                                 <textarea type={'text'} defaultValue={userData.bio}
-                                onChange={(e) => setBio(e.target.value)}></textarea>
-                            <button onClick={handleUpdate}>Valider</button>
+                                          onChange={(e) => setBio(e.target.value)}></textarea>
+                                <button onClick={handleUpdate}>Valider</button>
                             </>
                         )}
                     </div>
+                    <h4>Membre depuis le : {dateParser(userData.createdAt)}</h4>
+                    <h5 onClick={() => setFollowingPopup(true)}>Abonnements
+                        : {userData.following ? userData.following.length : ""}</h5>
+                    <h5 onClick={() => setFollowersPopup(true)}>Abonnés
+                        : {userData.followers ? userData.followers.length : ""}</h5>
+
                 </div>
             </div>
+            {followingPopup && (
+                <div className={'popup-profil-container'}>
+                    <div className={"modal"}>
+                        <h3>Abonnements</h3>
+                        <span className={"cross"} onClick={() => setFollowingPopup(false)}>&#10005;</span>
+                        <ul>
+                            {usersData.map((user) => {
+                                for (let i = 0; i < userData.following.length; i++) {
+                                    if (user._id === userData.following[i]) {
+                                        return (
+                                            <li key={user._id}>
+                                                <img src={user.picture} alt={'user-pic'}/>
+                                                <h4>{user.pseudo}</h4>
+                                                <h1>FOLLOW HANDLER</h1>
+                                            </li>
+                                        )
+                                    }
+                                }
+                            })}
+                        </ul>
+                    </div>
+                </div>)}
         </div>
     );
 };
