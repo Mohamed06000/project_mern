9dab0cc5b07d66e5bd8cb4efb941efc8a9762494
Mohamed Mazy <mazymohamed06@gmail.com>
2021-01-11 17:21:57 +0000

    add like and nomber of likers of post

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 //posts
 export const GET_POSTS = "GET_POSTS"
+export const LIKE_POSTS = "LIKE_POSTS"
+export const UNLIKE_POSTS = "UNLIKE_POSTS"
+
 
 export const getPosts = () => {
     return(dispatch) => {
@@ -16,4 +19,31 @@ export const getPosts = () => {
     }
 }
 
+export const likePost = (postId, userId) => {
+    return (dispatch) => {
+        return axios({
+            method: "patch",
+            url: `${process.env.REACT_APP_API_URL}api/post/like-post/` +postId,
+            data: {id: userId}
+        })
+            .then((err)=> {
+                dispatch({type: LIKE_POSTS, payload: {postId, userId}})
+            })
+            .catch((err) => console.log(err))
+    }
+}
+
+export const unlikePost = (postId, userId) => {
+    return (dispatch) => {
+        return axios({
+            method: "patch",
+            url: `${process.env.REACT_APP_API_URL}api/post/unlike-post/` +postId,
+            data: {id: userId}
+        })
+            .then((err)=> {
+                dispatch({type: UNLIKE_POSTS, payload: {postId, userId}})
+            })
+            .catch((err) => console.log(err))
+    }
+}
 //comments
